6011c455bfadc6e0c2235d47fb0d88413ac13e54
Endryus-Montezano <endryusmontezano@live.com>
2025-12-08 14:41:01 -0300

    Desenvolvendo o JS

---
+++ Dev Notes/js/scripts.js
@@ -0,0 +1,109 @@
+// Elementos
+const notesContainer = document.querySelector("#notes-container")
+const noteInput = document.querySelector("#note-content")
+const addNoteBtn = document.querySelector(".add-note")
+
+// Funções
+function showNotes() {
+    cleanNotes()
+
+    getNotes().forEach((note) => {
+        const noteElement = createNote(note.id, note.content, note.fixed)
+
+        notesContainer.appendChild(noteElement)
+    })
+}
+
+function cleanNotes() {
+    notesContainer.replaceChildren([])
+}
+
+/* Adiciona a nota */
+function addNote() {
+    const notes = getNotes()
+
+    const noteObject = {
+        id: generateId(),
+        content: noteInput.value,
+        fixed: false,
+    }
+    
+    const noteElement = createNote(noteObject.id, noteObject.content)
+
+    notesContainer.appendChild(noteElement)
+
+    notes.push(noteObject)
+
+    saveNotes(notes) 
+
+    noteInput.value = ""
+}
+
+/* Cria um id */
+function generateId() {
+    return Math.floor(Math.random() * 5000)
+}
+
+/* Adiciona o texto na nota  */
+function createNote(id,content, fixed) {
+    const element = document.createElement("div")
+
+    element.classList.add("note")
+
+    const textarea = document.createElement("textarea")
+
+    textarea.value = content
+
+    textarea.placeholder = "Adicione algum texto..."
+
+    element.appendChild(textarea)
+
+    const pinIcon = document.createElement("i")
+
+    pinIcon.classList.add(...["bi", "bi-pin"])
+
+    element.appendChild(pinIcon)
+
+    if(fixed) {
+        element.classList.add("fixed")
+    }
+
+    // Eventos do elemento
+    element.querySelector(".bi-pin").addEventListener("click", () => {
+        toggleFixNote(id)
+    })
+
+    return element
+}
+
+/* Nota fixa */
+function toggleFixNote(id) {
+    const notes = getNotes()
+
+    const targetNote = notes.filter((note) => note.id === id)[0]
+
+    targetNote.fixed = !targetNote.fixed
+
+    saveNotes(notes)
+
+    showNotes()
+}
+
+// Local storage
+function getNotes() {
+    const notes = JSON.parse(localStorage.getItem("notes") || "[]")
+
+    const orderedNotes = notes.sort((a, b) => (a.fixed > b.fixed ? -1 : 1))
+
+    return orderedNotes
+}
+
+function saveNotes(notes) {
+    localStorage.setItem("notes", JSON.stringify(notes))
+}
+
+// Eventos
+addNoteBtn.addEventListener("click", () => addNote())
+
+// Inicialização
+showNotes()
